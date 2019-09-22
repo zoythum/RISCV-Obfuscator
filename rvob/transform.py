aa48e76c9f48b44298b0c4ecf598ec3caf6822f9
Tomas Antonio Lopez <tomasantonio.lopez@mail.polimi.it>
2019-09-22 03:31:47 +0200

    Added support for procedure return jumps

--- rvob/transform.py
+++ rvob/transform.py
@@ -4,13 +4,13 @@ from bisect import bisect_right
 import networkx as nx
 import rvob.rep as rep
 
-jump_type = Enum('JUMP', 'U, C')
-# TODO what the hell do we do about 'jr' jumps?
+jump_type = Enum('JUMP', 'U, C, F, R')
 jump_ops = {
-    "call": jump_type.U,
+    "call": jump_type.F,
+    "jr": jump_type.R,
     "j": jump_type.U,
-    "jal": jump_type.U,
-    "jalr": jump_type.U,
+    "jal": jump_type.F,
+    "jalr": jump_type.F,
     "beq": jump_type.C,
     "beqz": jump_type.C,
     "bne": jump_type.C,
@@ -30,6 +30,7 @@ jump_ops = {
 }
 
 
+# TODO refactor this hell of a function, maybe make it recursive
 def build_cfg(src: rep.Source):
     label_dict = src.get_labels()
     # For now we only target '.text' sections
@@ -43,18 +44,20 @@ def build_cfg(src: rep.Source):
 
     # Load the exploration stack with the first line of the first code section, setting the root node as initial father
     exs = deque()
-    exs.append((sections[0][1], -1, frozenset()))
+    exs.append((sections[0][1], -1, frozenset(), -1))
 
     # Set the flag to temporarily ignore labels to false
     ignore_labels = False
 
     # Perform a DFS for each element on the exploration stack
     while exs.__len__() != 0:
-        dst, father, trail = exs.pop()
+        dst, father, trail, ra = exs.pop()
         # Extract section
         curr_sec = sections[bisect_right(pegs, dst) - 1]
         # Align local cursor
         cursor = dst - curr_sec[1]
+        # Initialize returning operation flag
+        ret = False
 
         # If a loop is detected, add the looping edge to the graph and continue with the next iteration
         if dst in trail:
@@ -71,7 +74,7 @@ def build_cfg(src: rep.Source):
             if statement.labels.__len__() != 0 and ignore_labels is False:
                 # Labeled statement encountered: set current line as block terminator and push the next line on exs
                 stop = curr_sec[1] + cursor
-                exs.append((stop, dst, trail))
+                exs.append((stop, dst, trail, ra))
                 ignore_labels = True
                 break
             elif type(statement) is rep.Instruction and jump_ops.keys().__contains__(statement.opcode):
@@ -80,15 +83,22 @@ def build_cfg(src: rep.Source):
 
                 # If conditional jump, push the line that follows on the exploration stack
                 if jump_type.C == jump_ops[statement.opcode]:
-                    exs.append((stop, dst, trail))
+                    exs.append((stop, dst, trail, ra))
 
                 # Push the jump's target onto the exploration stack
-                # TODO what the hell do we do about 'jr' jumps?
                 try:
-                    exs.append((label_dict[statement.instr_args["immediate"]], dst, trail))
+                    # If we are returning, set the `ret` flag
+                    if jump_ops[statement.opcode] == jump_type.R:
+                        ret = True
+                    else:
+                        # When performing a procedure call, save the return address on the appropriate stack
+                        if jump_ops[statement.opcode] == jump_type.F:
+                            exs.append((label_dict[statement.instr_args["immediate"]], dst, trail, stop))
+                        else:
+                            exs.append((label_dict[statement.instr_args["immediate"]], dst, trail, ra))
                 except KeyError:
                     # Jump to non-local code: execution resumes from the next line onwards
-                    exs.append((stop, dst, trail))
+                    exs.append((stop, dst, trail, ra))
                 break
                 
             cursor += 1
@@ -99,10 +109,14 @@ def build_cfg(src: rep.Source):
 
             # If this isn't the last section, push the next one's starting line on the exploration stack
             if curr_sec[2] != pegs[pegs.__len__() - 1]:
-                exs.append((pegs[bisect_right(pegs, curr_sec[2]) - 1], dst, trail))
+                exs.append((pegs[bisect_right(pegs, curr_sec[2]) - 1], dst, trail, ra))
 
         # Add the new block to the CFG
         cfg.add_node(dst, block=src.lines[dst:stop])
         cfg.add_edge(father, dst)
+        # If we are returning, build an arc to the code block after the procedure call
+        if ret is True:
+            cfg.add_edge(dst, bisect_right(pegs, ra) - 1)
+            ret = False
 
     return cfg
