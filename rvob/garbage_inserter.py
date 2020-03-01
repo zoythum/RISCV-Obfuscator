0965396b3849462c67c797c689e0a9bccc51765b
Mattia Iamundo <mattia.iamundo@mail.polimi.it>
2020-03-01 17:08:18 +0100

    Fix problem when an external node is selected, another will be selected

--- rvob/garbage_inserter.py
+++ rvob/garbage_inserter.py
@@ -1,6 +1,6 @@
 from networkx import DiGraph
 from random import randint, choices
-from analysis import free_regs_at
+from setup_structures import get_free_regs
 from structures import Register
 from rep.instruction_generator import garbage_inst
 
@@ -16,8 +16,10 @@ def insert_garbage_instr(cfg: DiGraph, node: int = None, block_size: int = None)
         block_size = randint(1, 10)
     if node is None:
         node = randint(1, cfg.number_of_nodes())
+    while 'external' in cfg.nodes[node]:
+        node = randint(1, cfg.number_of_nodes())
     line_num = randint(cfg.nodes[node]["block"].begin, cfg.nodes[node]["block"].end - 1)
-    free_regs = list(free_regs_at(line_num - 1, cfg))
+    free_regs = list(get_free_regs(cfg, line_num - 1))
     all_regs = list(map(lambda r: r.name.lower(), Register))
 
     instr_list = choices(list(garbage_inst.keys()), k=block_size)
