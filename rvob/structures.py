5884a7ee071ffb5b9755aca2077970d6c377fc7e
Mattia Iamundo <mattia.iamundo@mail.polimi.it>
2020-05-15 12:19:52 +0200

    [Fix] missing opcodes

--- rvob/structures.py
+++ rvob/structures.py
@@ -80,7 +80,7 @@ opcodes: Mapping[str, Tuple[int, bool]] = {
     'amomax.d': (3, True), 'amominu.d': (3, True), 'amomaxu.d': (3, True), 'jr': (1, False), 'j': (0, False),
     'beq': (2, False), 'bne': (2, False), 'blt': (2, False), 'bge': (2, False), 'ble': (2, False), 'bltu': (2, False),
     'bgeu': (2, False), 'sb': (2, False), 'sh': (2, False), 'sw': (2, False), 'sd': (2, False), 'li': (1, True),
-    'beqz': (1, False), 'bnez': (1, False), 'blez': (1, False), "bgt": (2, False), "not": (1, False),
+    'beqz': (1, False), 'bnez': (1, False), 'blez': (1, False), "bgt": (2, False), "not": (2, True), "snez": (2, True),
     'bgez': (1, False), 'bgtu': (2, False), 'bleu': (2, False), 'nop': (0, False), 'call': (0, False)
 }
 
