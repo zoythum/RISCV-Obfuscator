d7a914cfd479124833f8fc460477126c8235be30
Mattia Iamundo <mattia.iamundo@mail.polimi.it>
2020-12-18 18:24:48 +0100

    reformatted calc _fragmentation() to improve readability

--- rvob/benchmark.py
+++ rvob/benchmark.py
@@ -24,19 +24,19 @@ heat_file = None
 metrics_file = None
 trace_heat_map = None
 cfg: DiGraph = None
-mean_unp_life: int = 0
 
 
-def write_heat( first: bool, frag: Tuple[int, List[int], int, int, int] = None):
+def write_heat(first: bool, frag: Tuple[int, List[int], int, int, int] = None):
     global trace_heat_map
     global heat_file
     global metrics_file
-    global mean_unp_life
 
     for i in range(len(trace_heat_map.keys())):
-        heat_file.write(str(i) + ": " + str(trace_heat_map[i][0]) + " i:" + str(trace_heat_map[i][1].inserted) + " o_r: " + str(
-            trace_heat_map[i][1].o_reg) + " n_r: " + str(trace_heat_map[i][1].n_reg) + " opcd: " + str(trace_heat_map[i][1].op_code) + "\n")
-    mean_heat = [0]*len(Register)
+        heat_file.write(
+            str(i) + ": " + str(trace_heat_map[i][0]) + " i:" + str(trace_heat_map[i][1].inserted) + " o_r: " + str(
+                trace_heat_map[i][1].o_reg) + " n_r: " + str(trace_heat_map[i][1].n_reg) + " opcd: " + str(
+                trace_heat_map[i][1].op_code) + "\n")
+    mean_heat = [0] * len(Register)
     for val in trace_heat_map.values():
         for i in range(len(mean_heat)):
             mean_heat[i] += val[0][i]
@@ -44,7 +44,7 @@ def write_heat( first: bool, frag: Tuple[int, List[int], int, int, int] = None):
     for i in range(len(mean_heat)):
         mean_heat[i] //= trace_length
     if first:
-        metrics_file.write("Mean heat before: " + str(mean_heat)+"\n")
+        metrics_file.write("Mean heat before: " + str(mean_heat) + "\n")
     else:
         metrics_file.write("Mean heat after: " + str(mean_heat) + "\n")
 
@@ -53,94 +53,49 @@ def write_heat( first: bool, frag: Tuple[int, List[int], int, int, int] = None):
         # [0] mean fragmentation
         # [1] list of the fragmented ValueBlock with fragment number for each ValueBlock
         # [2] number of original ValueBlock
-        # [3] mean life (aggregated fragments)
-        # [4] mean life scrambled (single fragment)
-        metrics_file.write("Mean fragmentation: "+str(frag[0])+"\n")
-        metrics_file.write("Mean variable life (unprotected case): " + str(mean_unp_life) + "\n")
-        metrics_file.write("Mean variable life (frag accumulated): "+str(frag[3])+"\n")
-        metrics_file.write("Mean variable life (frag separated): " + str(frag[4]) + "\n")
+        metrics_file.write("Mean fragmentation: " + str(frag[0]) + "\n")
         metrics_file.write("Number of original ValueBlock: " + str(frag[2]) + "\n")
         metrics_file.write("List of fragmented ValueBlock: " + str(frag[1]) + "-> " + str(len(frag[1])) + "\n")
 
+
+def analyze_node(node: DiGraph.node, frag_dict: Dict[int, Set[Register]], frag_list: List[int]) -> int:
+    block_num: int = 0
+    for (reg, v) in node['reg_bind'].items():
+        v = [blk for blk in v if not blk.inserted]  # eliminate block added by some garbage instruction
+        for block in v:
+            # this check exclude the block that have the same group_id only because all of them start from the
+            # first line of the node, but they aren't part of the same original ValueBlock
+            if block.group_id is not None:
+                try:
+                    frag_dict[block.group_id].add(reg)
+                except KeyError:
+                    frag_dict[block.group_id] = {reg}
+                    block_num += 1
+            else:
+                block_num += 1
+                frag_list.append(1)
+    return block_num
 
 
 def calc_fragmentation():
     global cfg
 
-    mean_frag_list: List[int] = []
-    mean_life_scramb_list: List[int] = []
-    mean_life_list: List[int] = []
+    frag_list: List[int] = []  # indicates how many fragments have each block of the program
     block_num: int = 0
 
     for nd_id in cfg.nodes:
         node = cfg.nodes[nd_id]
+        frag_dict: Dict[int, Set[Register]] = {}
         if 'external' not in node:
-            frag_dict: Dict[int, Set[Register]] = {}
-            life_dict = {}
-            for it in node['reg_bind'].items():
-                k: Register = it[0]
-                v: List[ValueBlock] = it[1]
-                v = [blk for blk in v if not blk.inserted]  # eliminate block added by some garbage instruction
-                for el in v:
-                    mean_life_scramb_list.append(el.end_line - el.init_line + 1)
-                    # this check exclude the block that have the same group_id only because all of them start from the
-                    # first line of the node, but they aren't part of the same original ValueBlock
-                    if el.group_id is not None:
-                        try:
-                            frag_dict[el.group_id].add(k)
-                            life_dict[el.group_id] += (el.end_line - el.init_line + 1)
-                        except KeyError:
-                            frag_dict[el.group_id] = {k}
-                            life_dict[el.group_id] = (el.end_line - el.init_line + 1)
-                            block_num += 1
-                    else:
-                        block_num += 1
-                        mean_frag_list.append(1)
-                        mean_life_list.append((el.end_line - el.init_line + 1))
-        for val in frag_dict.values():
-            mean_frag_list.append(len(val))
-        for val in life_dict.values():
-            mean_life_list.append(val)
+            block_num += analyze_node(node, frag_dict, frag_list)
+            for val in frag_dict.values():
+                frag_list.append(len(val))
 
-    mean_fragmentation: int = 0
-    mean_life: int = 0
-    mean_life_scramb: int = 0
+    src_list = [elm for elm in frag_list if elm > 1]
+    mean_fragmentation = sum(frag_list)
+    mean_fragmentation //= len(frag_list)
 
-    src_list = []
-    for elm in mean_frag_list:
-        mean_fragmentation += elm
-        if elm > 1:
-            src_list.append(elm)
-    mean_fragmentation //= len(mean_frag_list)
-
-    for elm in mean_life_list:
-        mean_life += elm
-    mean_life //= len(mean_life_list)
-
-    for elm in mean_life_scramb_list:
-        mean_life_scramb += elm
-    mean_life_scramb //= len(mean_life_scramb_list)
-
-    return mean_fragmentation, src_list, block_num, mean_life, mean_life_scramb
-
-
-def calc_var_life():
-    global cfg
-    global mean_unp_life
-
-    life_list: List[int] = []
-    for n in cfg.nodes:
-        if 'external' not in cfg.nodes[n]:
-            reg_bind: Dict[Register, List[ValueBlock]] = cfg.nodes[n]['reg_bind']
-            for val in reg_bind.values():
-                for elm in val:
-                    life_list.append(elm.end_line - elm.init_line + 1)
-
-    mean_life: int = 0
-    for elm in life_list:
-        mean_life += elm
-
-    mean_unp_life = mean_life // len(life_list)
+    return mean_fragmentation, src_list, block_num
 
 
 def do_scrambling(iter_num: int):
@@ -163,14 +118,14 @@ def do_scrambling(iter_num: int):
         if ret == -1:
             failed_substitute += 1
 
-    print("Splitting failure rate: " + str(failed_splitting/iter_num * 100) + "%")
+    print("Splitting failure rate: " + str(failed_splitting / iter_num * 100) + "%")
     print("Substitution failure rate: " + str(failed_substitute / iter_num * 100) + "%")
 
 
 def do_iter():
     global cfg
     global heat_map
-    do_scrambling(1000)
+    do_scrambling(5)
     for t in range(5):
         failed = 0
         print("obfuscate iteration: " + str(t))
@@ -181,13 +136,13 @@ def do_iter():
             except (NotEnoughtRegisters, NotValidInstruction):
                 if z == 5:
                     failed += 1
-    print("Failure rate: " + str(failed/5 * 100) + "%")
+    print("Failure rate: " + str(failed / 5 * 100) + "%")
 
     for t in range(5):
         print("garbage iteration: " + str(t))
         garbage_inserter.insert_garbage_instr(cfg)
 
-    do_scrambling(1000)
+    # do_scrambling(1000)
 
 
 def benchmark(name: str, entry: str):
@@ -210,7 +165,6 @@ def benchmark(name: str, entry: str):
     rvob.setup_structures.organize_calls(cfg)
     rvob.registerbinder.bind_register_to_value(cfg)
     heat_map = heatmaps.register_heatmap(cfg, 50)
-    calc_var_life()
     dst = rel + '/benchmark/benchmark_output/' + name + ".txt"
     metric_dst = rel + '/benchmark/benchmark_output/' + name + "_metrics.txt"
     heat_file = open(dst, "w")
